1cc3d2ebd1cb32e4daf0af7a5a85949d1c2abafa
Diego Pontoriero <dpontor@gmail.com>
2014-04-16 18:52:57 -0700

    Not pretty, but it works.

--- Globber.hs
+++ Globber.hs
@@ -17,22 +17,9 @@ data MatchItem = MRange Char Char
                deriving Show
 
 matchGlob :: GlobPattern -> String -> Bool
-matchGlob rawPattern str =
-  let pattern = compilePattern rawPattern
-  in glob pattern str
+matchGlob pattern str = patternMatch (compilePattern pattern) str
 
-glob :: Glob -> String -> Bool
-glob (GAnyString:xs) (y:ys) = (glob xs (y:ys)) || (glob (GAnyString:xs) ys)
-glob (GAnyChar:xs) (_:ys) = glob xs ys
-glob (GLiteral x:xs) (y:ys) = (x == y) && glob xs ys
-glob (GSetMatch matchSet:xs) (y:ys) = setMatch matchSet y && glob xs ys
-glob [] [] = True
-glob _ _ = False
-
-setMatch :: MatchSet -> Char -> Bool
-setMatch matchSet char = any setMatch' matchSet
-  where setMatch' (MLiteral lit) = char == lit
-        setMatch' (MRange l r) = (char >= l) && (char <= r)
+-- Pattern compilation routines.
 
 compilePattern :: GlobPattern -> Glob
 compilePattern ('?':xs) = GAnyChar : compilePattern xs
@@ -40,7 +27,7 @@ compilePattern ('*':xs) = GAnyString : compilePattern xs
 compilePattern ('\\':x:xs) = GLiteral x : compilePattern xs
 compilePattern ('[':xs) =
   let (matchPart, rest) = splitMatchPart xs
-  in GSetMatch (matchSet matchPart) : compilePattern rest
+  in GSetMatch (makeMatchSet matchPart) : compilePattern rest
 compilePattern (x:xs) = GLiteral x : compilePattern xs
 compilePattern [] = []
 
@@ -51,12 +38,28 @@ splitMatchPart xs = let matchPart = extractMatchPart xs
 
 extractMatchPart :: GlobPattern -> GlobPattern
 extractMatchPart ('\\':x:xs) = '\\' : x : extractMatchPart xs
-extractMatchPart (']':xs) = []
+extractMatchPart (']':_) = []
 extractMatchPart (x:xs) = x : extractMatchPart xs
-extractMatchPart [] = error "unmatched [] in match set"
+extractMatchPart [] = error "unmatched ] in match set"
 
-matchSet :: GlobPattern -> MatchSet
-matchSet ('\\':x:xs) = MLiteral x : matchSet xs
-matchSet (x:'-':y:xs) = MRange x y : matchSet xs
-matchSet (x:xs) = MLiteral x : matchSet xs
-matchSet [] = []
+makeMatchSet :: GlobPattern -> MatchSet
+makeMatchSet ('\\':x:xs) = MLiteral x : makeMatchSet xs
+makeMatchSet (x:'-':y:xs) = MRange x y : makeMatchSet xs
+makeMatchSet (x:xs) = MLiteral x : makeMatchSet xs
+makeMatchSet [] = []
+
+-- Matching routines.
+
+patternMatch :: Glob -> String -> Bool
+patternMatch (GAnyString:xs) (y:ys) =
+  (patternMatch xs (y:ys)) || (patternMatch (GAnyString:xs) ys) || (patternMatch xs ys)
+patternMatch (GAnyChar:xs) (_:ys) = patternMatch xs ys
+patternMatch (GLiteral x:xs) (y:ys) = (x == y) && patternMatch xs ys
+patternMatch (GSetMatch matchSet:xs) (y:ys) = setMatch matchSet y && patternMatch xs ys
+patternMatch [] [] = True
+patternMatch _ _ = False
+
+setMatch :: MatchSet -> Char -> Bool
+setMatch set char = any setMatch' set
+  where setMatch' (MLiteral lit) = char == lit
+        setMatch' (MRange l r) = (char >= l) && (char <= r)
